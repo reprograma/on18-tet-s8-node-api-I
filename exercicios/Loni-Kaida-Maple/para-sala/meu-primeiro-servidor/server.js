a9c3fb571edc8a4c31f4639c21e66539a277d3d2
LoniMaple <joshu4drake@gmail.com>
2022-10-01 17:04:04 -0300

    server 1.0a

--- exercicios/Loni-Kaida-Maple/para-sala/meu-primeiro-servidor/server.js
+++ exercicios/Loni-Kaida-Maple/para-sala/meu-primeiro-servidor/server.js
@@ -1,1 +1,24 @@
-console.log("nosso codigo aqui")
+const express = require("express");
+const doushijinJson = require("./data/sunrise.json");
+
+console.log("🐼🐼🐼");
+
+const app = express();
+
+app.get("/", (request, response)=>{
+    response.status(200).json(["Gundam Type 78"])
+})
+
+app.get("/doushijin", (request, response)=>{
+    response.status(200).json({
+        mensagem: "mangas",
+        data:doushijinJson
+    })
+
+    
+})
+
+
+app.listen(8080, ()=>{
+    console.log("Nya is Running");
+})
