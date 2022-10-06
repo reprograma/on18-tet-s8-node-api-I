7b09299569f84c09181234e18a25437d24dd1d62
ghizieri <ghizieri@gmail.com>
2022-10-06 10:48:26 -0300

    terminando o exercício da semana 8

--- exercicios/para-casa/doc.md
+++ exercicios/para-casa/doc.md
@@ -1,1 +1,22 @@
-# documentação
+## Documentação
+
+- [GET] "/filmes/" - retornar todos os filmes
+
+- [GET] "/filmes/por_id/:id" - retorna o filme com id selecionado
+
+- [GET] "/filmes/por_nome?nome=""" - retorna o filme com nome selecionado
+
+- [GET]"/filmes/por_chave_valor?chave=insira_chave&valor=insira_valor" - retorna os filmes que atendem ao critério passado por chave e valor
+
+- [POST]"/filmes/cadastrar" - cadastra um novo filme
+
+
+- [GET] "/series/" - retornar todos as séries
+
+- [GET] "/series/por_id/:id" - retorna a série com id selecionado
+
+- [GET] "/series/por_nome?nome=""" - retorna a série com nome selecionado
+
+- [GET]"/series/por_chave_valor?chave=insira_chave&valor=insira_valor" - retorna as séries que atendem ao critério passado por chave e valor
+
+- [POST]"/series/cadastrar" - cadastra uma nova série
--- exercicios/para-casa/server.js
+++ exercicios/para-casa/server.js
@@ -11,27 +11,21 @@ function bancoDeDados() {
 }
   
 const express = require("express");
-const fs = require("fs");
 const app = express();
 app.use(express.json())
 
-const series = require("./data/series.json");
-const filmes=  require("./data/filmes.json");
-
-console.log("filmes: " + filmes);
 app.get('/filmes', async (req, res) => {
     const bd = await bancoDeDados();
     res.json(bd.filmes);
 });
 
-app.get('/filmes/buscar/:id', async (req, res) => {
+app.get('/filmes/por_id/:id', async (req, res) => {
     const bd = await bancoDeDados();
-    const id = +req.params["id"];
+    const id = +req.params.id;
     const filme = bd.filmes.find( f => f.id === id);
 
     if( !filme ) {
         res.status(404);
-        //res.sendStatus(404);
         res.send("Filme não encontrado");
     }
     else {
@@ -39,75 +33,169 @@ app.get('/filmes/buscar/:id', async (req, res) => {
     }
 });
 
-app.get('/filmes/buscar/:nome', async (req, res) => {
+app.get('/filmes/por_nome/', async (req, res) => {
     const bd = await bancoDeDados();
-    const nome = req.params["nome"].toLowerCase();;
-    const filme = bd.filmes.find( f => f.title.toLowerCase() === nome);
+    const nome = req.query.nome.toLowerCase();;
+    const filme = bd.filmes.find( f => f.Title.toLowerCase() === nome);
 
     if( !filme ) {
         res.status(404);
-        //res.sendStatus(404);
         res.send("Filme não encontrado");
-    }
-    res.send(filme);
-});
-
-app.get('/filmes/buscar/por_chave_valor/:chave/valor/:valor', async (req, res) => {
-    const bd = await bancoDeDados();
-    const chave = req.params["chave"];
-    const valor = req.params["valor"];
-    const filme = bd.filmes.find( f => f[chave] == valor);
-
-    if( !filme ) {
-        res.status(404);
-        //res.sendStatus(404);
-        res.send("Filme não encontrado");
-    }
-    res.send(filme);
-});
-
-app.get('/filmes/buscar/:id', (req, res) => {
-    const id = +req.params["id"];
-    const filme = filmes.find( f => f.id === id);
-
-    if( !filme ) {
-        res.status(404);
-        //res.sendStatus(404);
-        res.send("Filme não encontrado");
-    }
-    else {
+    } else {
         res.json(filme);
     }
+    
 });
 
-app.get('/filmes/buscar/:nome', async (req, res) => {
-    const nome = req.params["nome"].toLowerCase();;
-    const filme = filmes.find( f => f.Title.toLowerCase() === nome);
+app.get('/filmes/por_chave_valor', async (req, res) => {
+    const bd = await bancoDeDados();
+    const chave = req.query.chave.toLowerCase();
+    const valor = req.query.valor.toLowerCase();
+    const filmes = bd.filmes.filter( f => {
+        for (const chaveObjeto of Object.keys(f)) {
+            if( chaveObjeto.toLowerCase() == chave) {
+                const valorString = ""+ f[chaveObjeto];
+                return valorString.toLowerCase() == valor;
+            }
+        }
+        return false;
+    });
 
-    if( !filme ) {
+    if( filmes.length == 0 ) {
         res.status(404);
-        //res.sendStatus(404);
-        res.send("Filme não encontrado");
+        res.json("Filme não encontrado");
+    } else {
+        res.json(filmes);
     }
-    res.send(filme);
+    
 });
 
-app.get('/filmes/buscar/por_chave_valor/:chave/valor/:valor', async (req, res) => {
-    const chave = req.params["chave"];
-    const valor = req.params["valor"];
-    const filme = filmes.find( f => f[chave] == valor);
+app.post("/filmes/cadastrar", async (request, response)=>{
+    try {
+        let bodyRequest = request.body
+        let dbFilmes = await bancoDeDados()
+        let filmes = dbFilmes.filmes
 
-    if( !filme ) {
+        if( !("title" in bodyRequest) ) {
+            response.status(400).json({message: "título faltando."});
+            return;
+        }
+
+        if( !("description" in bodyRequest) ) {
+            response.status(400).json({message: "descrição faltando."});
+            return;
+        }
+
+        let novoFilme = {
+            id:(filmes.length)+1,
+            title: bodyRequest.title,
+            description: bodyRequest.description
+        }
+
+        filmes.push(novoFilme)
+
+        response.status(201).send({
+            mensagem: "filme cadastrado com sucesso",
+            novoFilme
+        })
+    } catch {
+        res.status(500).json({message: "Houve um erro ao cadastrar filme."});
+    }
+
+})
+
+app.get('/series', async (req, res) => {
+    const bd = await bancoDeDados();
+    res.json(bd.series);
+});
+
+app.get('/series/por_id/:id', async (req, res) => {
+    const bd = await bancoDeDados();
+    const id = req.params.id;
+    const serie = bd.series.find( s => s.id === id);
+
+    if( !serie ) {
         res.status(404);
-        //res.sendStatus(404);
-        res.send("Filme não encontrado");
+        res.send("Série não encontrada");
     }
-    res.send(filme);
+    else {
+        res.json(serie);
+    }
 });
 
-app.get('/filmes/cadastrar', async (req, res) => {
-    //res.send(filmes);
+app.get('/series/por_nome/', async (req, res) => {
+    const bd = await bancoDeDados();
+    const nome = req.query.nome.toLowerCase();;
+    const serie = bd.series.find( f => f.title.toLowerCase() === nome);
+
+    if( !serie ) {
+        res.status(404);
+        res.send("Série não encontrada");
+    } else {
+        res.json(serie);
+    }
+    
 });
+
+app.get('/series/por_chave_valor', async (req, res) => {
+    const bd = await bancoDeDados();
+    const chave = req.query.chave.toLowerCase();
+    const valor = req.query.valor.toLowerCase();
+    const series = bd.series.filter( s => {
+        for (const chaveObjeto of Object.keys(s)) {
+            if( chaveObjeto.toLowerCase() == chave) {
+                const valorString = ""+ s[chaveObjeto];
+                return valorString.toLowerCase() == valor;
+            }
+        }
+        return false;
+    });
+
+    if( series.length == 0 ) {
+        res.status(404);
+        res.json("Séries não encontradas");
+    } else {
+        res.json(series);
+    }
+    
+});
+
+app.post("/series/cadastrar", async (request, res)=>{
+    try {
+        let bodyRequest = request.body
+        let dbFilmes = await bancoDeDados()
+        let series = dbFilmes.series
+
+        console.log(series.length)
+
+        if( !("title" in bodyRequest) ) {
+            res.status(400).json({message: "título faltando."});
+            return;
+        }
+
+        if( !("description" in bodyRequest) ) {
+            res.status(400).json({message: "descrição faltando."});
+            return;
+        }
+
+        let novaSerie = {
+            id:(series.length)+1,
+            title: bodyRequest.title,
+            description: bodyRequest.description
+        }
+
+        series.push(novaSerie)
+
+        res.status(201).send({
+            mensagem: "serie cadastrada com sucesso",
+            novaSerie
+        })
+
+    } catch {
+        res.status(500).json({message: "Houve um erro ao cadastrar filme."});
+    }
+
+})
   
 const port = 3000;
 app.listen(port, () => {
