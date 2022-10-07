b1f100892c3fce464ad84aecfee33923d352dce6
gabisnasc <gabriellesantos1804@outlook.com>
2022-10-06 21:27:32 -0300

    feito

--- exercicios/para-sala/meu-primeiro-servidor/server.js
+++ exercicios/para-sala/meu-primeiro-servidor/server.js
@@ -1,1 +1,13 @@
-console.log("nosso codigo aqui")
+const express = require ("express")
+
+//executar 
+const app = express()
+
+app.get("/", (request, response)=>{
+    response.status(200).json(["JS é muito legal uhuuul somos backenderssss"])
+})
+
+//iniciando o servidor
+app.listen(8080, ()=>{
+    console.log("Servidor rodando lindo bonito e legal")
+})
--- exercicios/para-sala/servidor-em-aula/server.js
+++ exercicios/para-sala/servidor-em-aula/server.js
@@ -1,1 +1,47 @@
-console.log("nosso codigo aqui")
+const filmesJson = require(".data/ghibli.json")
+const express = require("express")
+const app = express()
+app.use(expresse.json())
+
+app.get("/filmes", (request, response)=>{
+    response.status(200).json({
+        mensagem:"ta funcionando",
+    data:filmesJson    })
+})
+
+// app.listen(3030, ()=>{
+//     console.log("Alô, pepe moreno")
+// })
+
+app.get("/filmes/:id",(request, response)=>{
+    let idRequest = request.params.id
+    let filmeEncontrado = filmesJson.find(filme => filme.id == idRequest)
+    response.status(200).send(filmeEncontrado)
+})
+
+app.get("/filmes/pesquisar",(request, response)=>{
+    
+    let tituloRequest = request.query.titulo.toLowerCase()
+    let filmeEncontrado = filmesJson.filter(filme => filme.title.toLowerCase().includes(tituloRequest))
+        
+    })
+
+app.post("/filmes/cadastrar", (request, response)=>{
+    let bodyRequest = request.body
+    let novoFilme={
+        id:bodyRequest.id,
+        title:bodyRequest.title,
+        description: bodyRequest.description
+
+    }
+filmesJson.push(novoFilme)
+
+    response.status(201).send({
+        mensagem: "filme cadastrado com sucesso",
+        data: novoFilme
+    })
+})
+
+app.listen(3030, ()=>{
+    console.log("Alô, Pepe moreno?")
+})
